fdd2bcaf31c5d8f936014442c849b1aa61a30459
Joseph Niestroy <106636917+JoeyNiestroy@users.noreply.github.com>
2024-05-10 21:10:49 -0400

    Update Actor_Model.py

--- Actor_Model.py
+++ Actor_Model.py
@@ -9,264 +9,7 @@ import time
 import torch.nn.functional as F
 from st_moe_pytorch import MoE
 
-def coral_loss(logits, levels, importance_weights=None, reduction='mean'):
-    """Computes the CORAL loss described in
 
-    Cao, Mirjalili, and Raschka (2020)
-    *Rank Consistent Ordinal Regression for Neural Networks
-       with Application to Age Estimation*
-    Pattern Recognition Letters, https://doi.org/10.1016/j.patrec.2020.11.008
-
-    Parameters
-    ----------
-    logits : torch.tensor, shape(num_examples, num_classes-1)
-        Outputs of the CORAL layer.
-
-    levels : torch.tensor, shape(num_examples, num_classes-1)
-        True labels represented as extended binary vectors
-        (via `coral_pytorch.dataset.levels_from_labelbatch`).
-
-    importance_weights : torch.tensor, shape=(num_classes-1,) (default=None)
-        Optional weights for the different labels in levels.
-        A tensor of ones, i.e.,
-        `torch.ones(num_classes-1, dtype=torch.float32)`
-        will result in uniform weights that have the same effect as None.
-
-    reduction : str or None (default='mean')
-        If 'mean' or 'sum', returns the averaged or summed loss value across
-        all data points (rows) in logits. If None, returns a vector of
-        shape (num_examples,)
-
-    Returns
-    ----------
-        loss : torch.tensor
-        A torch.tensor containing a single loss value (if `reduction='mean'` or '`sum'`)
-        or a loss value for each data record (if `reduction=None`).
-
-    Examples
-    ----------
-    >>> import torch
-    >>> from coral_pytorch.losses import coral_loss
-    >>> levels = torch.tensor(
-    ...    [[1., 1., 0., 0.],
-    ...     [1., 0., 0., 0.],
-    ...    [1., 1., 1., 1.]])
-    >>> logits = torch.tensor(
-    ...    [[2.1, 1.8, -2.1, -1.8],
-    ...     [1.9, -1., -1.5, -1.3],
-    ...     [1.9, 1.8, 1.7, 1.6]])
-    >>> coral_loss(logits, levels)
-    tensor(0.6920)
-    """
-
-    if not logits.shape == levels.shape:
-        raise ValueError("Please ensure that logits (%s) has the same shape as levels (%s). "
-                         % (logits.shape, levels.shape))
-
-    term1 = (F.logsigmoid(logits)*levels
-                      + (F.logsigmoid(logits) - logits)*(1-levels))
-
-    if importance_weights is not None:
-        term1 *= importance_weights
-
-    val = (-torch.sum(term1, dim=1))
-
-    if reduction == 'mean':
-        loss = torch.mean(val)
-    elif reduction == 'sum':
-        loss = torch.sum(val)
-    elif reduction is None:
-        loss = val
-    else:
-        s = ('Invalid value for `reduction`. Should be "mean", '
-             '"sum", or None. Got %s' % reduction)
-        raise ValueError(s)
-
-    return loss
-
-
-def corn_loss(logits, y_train, num_classes):
-    """Computes the CORN loss described in our forthcoming
-    'Deep Neural Networks for Rank Consistent Ordinal
-    Regression based on Conditional Probabilities'
-    manuscript.
-
-    Parameters
-    ----------
-    logits : torch.tensor, shape=(num_examples, num_classes-1)
-        Outputs of the CORN layer.
-
-    y_train : torch.tensor, shape=(num_examples)
-        Torch tensor containing the class labels.
-
-    num_classes : int
-        Number of unique class labels (class labels should start at 0).
-
-    Returns
-    ----------
-        loss : torch.tensor
-        A torch.tensor containing a single loss value.
-
-    Examples
-    ----------
-    >>> import torch
-    >>> from coral_pytorch.losses import corn_loss
-    >>> # Consider 8 training examples
-    >>> _  = torch.manual_seed(123)
-    >>> X_train = torch.rand(8, 99)
-    >>> y_train = torch.tensor([0, 1, 2, 2, 2, 3, 4, 4])
-    >>> NUM_CLASSES = 5
-    >>> #
-    >>> #
-    >>> # def __init__(self):
-    >>> corn_net = torch.nn.Linear(99, NUM_CLASSES-1)
-    >>> #
-    >>> #
-    >>> # def forward(self, X_train):
-    >>> logits = corn_net(X_train)
-    >>> logits.shape
-    torch.Size([8, 4])
-    >>> corn_loss(logits, y_train, NUM_CLASSES)
-    tensor(0.7127, grad_fn=<DivBackward0>)
-    """
-    sets = []
-    for i in range(num_classes-1):
-        label_mask = y_train > i-1
-        label_tensor = (y_train[label_mask] > i).to(torch.int64)
-        sets.append((label_mask, label_tensor))
-
-    num_examples = 0
-    losses = 0.
-    for task_index, s in enumerate(sets):
-        train_examples = s[0]
-        train_labels = s[1]
-
-        if len(train_labels) < 1:
-            continue
-
-        num_examples += len(train_labels)
-        pred = logits[train_examples, task_index]
-
-        loss = -torch.sum(F.logsigmoid(pred)*train_labels
-                          + (F.logsigmoid(pred) - pred)*(1-train_labels))
-        losses += loss
-
-    return losses/num_examples
-
-
-class CoralLoss(torch.nn.Module):
-    """Computes the CORAL loss described in
-
-    Cao, Mirjalili, and Raschka (2020)
-    *Rank Consistent Ordinal Regression for Neural Networks
-       with Application to Age Estimation*
-    Pattern Recognition Letters, https://doi.org/10.1016/j.patrec.2020.11.008
-
-    Parameters
-    ----------
-    reduction : str or None (default='mean')
-        If 'mean' or 'sum', returns the averaged or summed loss value across
-        all data points (rows) in logits. If None, returns a vector of
-        shape (num_examples,)
-
-    Examples
-    ----------
-    >>> import torch
-    >>> from coral_pytorch.losses import CoralLoss
-    >>> levels = torch.tensor(
-    ...    [[1., 1., 0., 0.],
-    ...     [1., 0., 0., 0.],
-    ...    [1., 1., 1., 1.]])
-    >>> logits = torch.tensor(
-    ...    [[2.1, 1.8, -2.1, -1.8],
-    ...     [1.9, -1., -1.5, -1.3],
-    ...     [1.9, 1.8, 1.7, 1.6]])
-    >>> loss = CoralLoss()
-    >>> loss(logits, levels)
-    tensor(0.6920)
-    """
-
-    def __init__(self, reduction='mean'):
-        super().__init__()
-        self.reduction = reduction
-
-    def forward(self, logits, levels, importance_weights=None):
-        """
-        Parameters
-        ----------
-        logits : torch.tensor, shape(num_examples, num_classes-1)
-            Outputs of the CORAL layer.
-
-        levels : torch.tensor, shape(num_examples, num_classes-1)
-            True labels represented as extended binary vectors
-            (via `coral_pytorch.dataset.levels_from_labelbatch`).
-
-        importance_weights : torch.tensor, shape=(num_classes-1,) (default=None)
-            Optional weights for the different labels in levels.
-            A tensor of ones, i.e.,
-            `torch.ones(num_classes-1, dtype=torch.float32)`
-            will result in uniform weights that have the same effect as None.
-        """
-        return coral_loss(
-            logits, levels,
-            importance_weights=importance_weights,
-            reduction=self.reduction)
-
-
-class CornLoss(torch.nn.Module):
-    """
-    Computes the CORN loss described in our forthcoming
-    'Deep Neural Networks for Rank Consistent Ordinal
-    Regression based on Conditional Probabilities'
-    manuscript.
-
-    Parameters
-    ----------
-    num_classes : int
-        Number of unique class labels (class labels should start at 0).
-
-    Examples
-    ----------
-    >>> import torch
-    >>> from coral_pytorch.losses import corn_loss
-    >>> # Consider 8 training examples
-    >>> _  = torch.manual_seed(123)
-    >>> X_train = torch.rand(8, 99)
-    >>> y_train = torch.tensor([0, 1, 2, 2, 2, 3, 4, 4])
-    >>> NUM_CLASSES = 5
-    >>> #
-    >>> #
-    >>> # def __init__(self):
-    >>> corn_net = torch.nn.Linear(99, NUM_CLASSES-1)
-    >>> #
-    >>> #
-    >>> # def forward(self, X_train):
-    >>> logits = corn_net(X_train)
-    >>> logits.shape
-    torch.Size([8, 4])
-    >>> corn_loss(logits, y_train, NUM_CLASSES)
-    tensor(0.7127, grad_fn=<DivBackward0>)
-    """
-    def __init__(self, num_classes):
-        super().__init__()
-        self.num_classes = num_classes
-
-    def forward(self, logits, y_train):
-        """
-        Parameters
-        ----------
-        logits : torch.tensor, shape=(num_examples, num_classes-1)
-            Outputs of the CORN layer.
-
-        y_train : torch.tensor, shape=(num_examples)
-            Torch tensor containing the class labels.
-
-        Returns
-        ----------
-        loss : torch.tensor
-            A torch.tensor containing a single loss value.
-        """
-        return corn_loss(logits, y_train, num_classes=self.num_classes)
 
 class RMSNorm(torch.nn.Module):
     def __init__(self, dim: int, eps: float = 1e-6):
@@ -296,15 +39,14 @@ class Chess_Model_Actor(nn.Module):
         self.value_ff = nn.Linear(dim, dim)
         self.action_ff = nn.Linear(dim, dim)
         self.action_value_ff = nn.Linear(dim, dim)
-        # self.layer_norm = nn.LayerNorm(dim)
+
         self.output_layer1 = nn.Linear(dim*65,1968)  # Output layer 1
-        # self.output_layer2 = nn.Linear(dim*65,1968*63)  # Output layer 2
+
         self.output_layer3 = nn.Linear(dim,14)
         self.reward_layer = nn.Linear(dim*65,1)
         self.tanh = nn.Tanh()
         self.gelu = nn.GELU()
-        # self.value_densenet = DenseNet1D(num_classes= 32)
-        # self.action_densenet = DenseNet1D(num_classes= 4097)
+
 
 
     def forward(self, input1, input2, mask):
@@ -315,18 +57,16 @@ class Chess_Model_Actor(nn.Module):
         encoded = self.transformer_encoder(embedded) # Pass added embeddings through Transformer encoder
 
         
-        # encoded = self.layer_norm(encoded)
 
 
-        #Predicting best moves
-        # output1_temp =self.action_ff(encoded_action)
+
+
 
         output1 = self.output_layer1(torch.flatten(encoded , start_dim=1)) #torch.flatten(encoded, start_dim=1)
-        mask_unsqueezed = mask < -1#.unsqueeze(1)  # Now shape is (batch_size, 1, 4097)
-        # mask_repeated = mask_unsqueezed.repeat(1, 65, 1) < -1
+        mask_unsqueezed = mask < -1
 
 
-        # output1 = output1 + mask # Apply output layer 1 to encoded features
+        
 
         _MASKING_VALUE = -1e+30 if output1.dtype == torch.float32 else -1e+4
 
@@ -334,33 +74,25 @@ class Chess_Model_Actor(nn.Module):
 
         dense_for_value_forward = encoded[:,-1,:].clone()
 
-        #Predicting Action Values
-        # output2_temp = self.action_value_ff(encoded[:,-1,:])
-        # output2 = self.output_layer2(torch.flatten(encoded , start_dim=1))
-        action_values_output = None#output2.view(-1, 1968, 63)
+
+        action_values_output = None
 
 
-        #Predicting Values
+
         output3 = self.output_layer3(encoded)
-        # values_temp = self.value_ff(encoded[:,-1,:])
-        # values_temp = nn.ELU()(values_temp)
 
 
-        #Predicting Values
-        # values_temp = self.value_ff(encoded[:,-1,:])
-        # print(encoded.shape)
-        # print(encoded[:,-1,:].unsqueeze(1).shape)
-        # values = self.value_densenet(encoded[:,-1,:].unsqueeze(1))
+
+
         values = self.tanh(self.reward_layer(torch.flatten(encoded , start_dim=1)))
-        # values_post_tan = self.tanh(values)
-        # values = nn.Sigmoid()(values_temp)
+        
         return output1_final , action_values_output  , output3 ,encoded[:,-1,:], values
 
 
 from torch.utils.data import Dataset, DataLoader
 import torch
 
-class MyDataset(Dataset):
+class Dataset(Dataset):
     def __init__(self, data, targets, mask, targets_move, values):
         """
         Args:
@@ -422,31 +154,9 @@ if __name__ == '__main__':
 
     model.to(device)
     model.train()
-    # model.half()
 
 
-
-    # features = torch.load("/media/joey/Elements/trainingdata.pt")
-    # features = features
-    # targets = torch.load("/media/joey/Elements/trainingtargets.pt")
-    # mask = torch.load("/media/joey/Elements/trainingdata_mask.pt")
-    # target_moves = torch.load("/media/joey/Elements/trainingtargets_moves.pt")
-    # target_values = torch.load("/media/joey/Elements/trainingtags.pt")
-
-    # targets_flat = targets[:,-1]
-
-    # class_counts = torch.zeros(4097)
-    # for i in range(4096):
-    #     class_counts[i] = (targets_flat == i).sum()
-
-    # default_weight = 1.0  # This could be set to the mean of the non-zero weights, for example
-    # inverse_weights = torch.where(class_counts > 0, 1.0 / class_counts, torch.tensor(default_weight))
-    # normalized_weights = 4097 * inverse_weights / inverse_weights.sum()
-    # normalized_weights = normalized_weights.to(device)
-
-    # my_dataset = MyDataset(data=features, targets=targets, mask= mask, targets_move = target_moves, values= target_values)
-
-    batch_size = 128  # Choose a batch size that fits your needs
+    batch_size = 128 
 
     # data_loader = DataLoader(my_dataset, batch_size=batch_size, shuffle=True)
 
@@ -455,8 +165,7 @@ if __name__ == '__main__':
     criterion2 = CornLoss(num_classes=64)#nn.CrossEntropyLoss(ignore_index=32) #pos_weight = normalized_weights)
     criterion3 = nn.MSELoss()#CornLoss(num_classes=128)#nn.CrossEntropyLoss(ignore_index=1968) #nn.MSELoss() #
     criterion4 = nn.CrossEntropyLoss(ignore_index=13)
-    optimizer =  optim.AdamW(model.parameters(), lr = 1e-4,betas=(0.9, 0.999), weight_decay= 1e-2)    #optim.SGD(model.parameters(), lr = 1e-3, momentum=.9)#optim.Adam(model.parameters(), lr = 5e-4,betas=(0.9, 0.999))  # Feel free to use any optimizer
-#optim.Adagrad(model.parameters(), lr = 1e-3)
+    optimizer =  optim.AdamW(model.parameters(), lr = 1e-4,betas=(0.9, 0.999), weight_decay= 1e-2) 
 
 
 
@@ -467,17 +176,13 @@ if __name__ == '__main__':
             mask = torch.load("/media/joey/Elements/Data_Fen/trainingdata_mask"+ str(epoch) + ".pt")
             target_moves = torch.load("/media/joey/Elements/Data_Fen/trainingtargets_moves"+ str(epoch) + ".pt")
             target_values = torch.load("/media/joey/Elements/Data_Fen/trainingtags"+ str(epoch) + ".pt")
-            my_dataset = MyDataset(data=features, targets=targets, mask= mask, targets_move = target_moves, values= target_values)
+            my_dataset = Dataset(data=features, targets=targets, mask= mask, targets_move = target_moves, values= target_values)
             data_loader = DataLoader(my_dataset, batch_size=batch_size, shuffle=True)
 
             batch_count = 0
 
             grad_steps = 16
 
-            # if epoch > 15:
-            #     grad_steps = 2
-            # else:
-            #     grad_steps = 2
 
             progress_bar = tqdm(range(len(data_loader)))
 
@@ -508,9 +213,7 @@ if __name__ == '__main__':
 
                 output1, output2, output3, dense, predicted_values = model(data[:,0,:], data[:,1,:] , mask)
 
-                # print(output1[:,-1,:].shape)
-                # print(target[:,-1].shape)
-                # model.close()
+
 
                 if batch_size == 1:
                     output1 = output1.squeeze(0)
@@ -521,39 +224,20 @@ if __name__ == '__main__':
 
                 else:
                     target = target[:,-1]
-                    # output1 = output1.view(-1, 1968)  # Flatten to [128*65, 4097]
-                    # target = target.view(-1)
+
 
 
                     loss1 = criterion(output1, target.view(-1))
                     loss1_temp = loss1.item()
 
-                    # output2 = output2.view(-1, 63)
-                    # print(output2.shape)
-                    # print(final_binned_masked_action_values.view(-1).shape)
-                    # output2 = output2.view(-1, 8)
 
 
-                    # mask = final_binned_masked_action_values.view(-1) < 80
-
-
-
-                    # # Applying the mask
-                    # selected_logits = output2[mask]
-                    # final_values_actions = final_binned_masked_action_values.view(-1)[mask]
-
-
-                    # print(selected_logits.shape)
-                    # print(final_values_actions.shape)
-                    # final_binned_masked_action_values = final_binned_masked_action_values.view(-1)
-                    # loss2 = criterion2(selected_logits, final_values_actions )
-                    loss2_float = 1#loss2.item()
+                    loss2_float = 1
 
 
 
 
                     loss3 = criterion3(predicted_values, values) #final_values.view(-1)
-                    # penalty = (1 - torch.abs(predicted_values).mean())
                     loss3_float = loss3.item()
 
 
@@ -565,11 +249,7 @@ if __name__ == '__main__':
                     loss4_float = loss4.item()
 
 
-                    # loss4 = .5* loss4
-                    # adjusted_penalty = 1*penalty
-                    # loss3 = loss3 + adjusted_penalty
 
-                # total_loss = loss1  + loss2
 
 
                 batch_count += 1
@@ -579,16 +259,11 @@ if __name__ == '__main__':
                         pass
                 else:
 
-                    # total_loss.backward()
-                    # loss1.backward(retain_graph=True)
-                    # loss2.backward() #retain_graph=True)
-                    loss1.backward()
-                    # loss3.backward
 
-                    
-                    # loss2.backward()
-                    # if epoch > 15:
-                    #     torch.nn.utils.clip_grad_value_(model.parameters(), 3.0)
+                    loss1.backward()
+
+
+
 
                     avg.append(loss1_temp)
                     loss3_temp = loss3_float
@@ -601,11 +276,6 @@ if __name__ == '__main__':
                     if batch_count % grad_steps == 0 and epoch != 26:
                         optimizer.step()
                         optimizer.zero_grad()
-
-                # if batch_count % 600 == 0:
-                #     time.sleep(60)
-                # if batch_count % 1000 == 0:
-                #     torch.save(model.state_dict(), 'Actor_basic_'+str(epoch)+'.pth')
 
 
             torch.save(model.state_dict(), '/media/joey/Elements/Actor_basic_medium_'+str(epoch)+'.pth')
